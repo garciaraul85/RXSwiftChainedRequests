5cd445717a8e7baadab9d5f86a63195fc1f0736f
raulgarcia <garcia.raul@gmail.com>
2020-09-22 13:41:21 -0400

    Network service connected to tableview in controller

---
+++ RXSwiftChainedRequests/BranchTableViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  BranchTableViewCell.swift
+//  RXSwiftChainedRequests
+//
+//  Created by Saulo Garcia on 9/22/20.
+//
+
+import UIKit
+
+class BranchTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var branchNameLabel: UILabel!
+    
+}
--- RXSwiftChainedRequests/ViewController.swift
+++ RXSwiftChainedRequests/ViewController.swift
@@ -7,12 +7,30 @@
 
 import UIKit
 import RxSwift
+import RxCocoa
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    private let githubRepository = GithubRepository()
+    private let disposeBag = DisposeBag()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        chainNetworkCalls()
     }
-
+    
+    func chainNetworkCalls() {
+        githubRepository
+            .getRepos()
+            .flatMap { repos -> Observable<[Branch]> in
+                let randomNumber = Int.random(in: 0...50)
+                let repo = repos[randomNumber]
+                return self.githubRepository.getBranches(owner: repo.owner.login, repoName: repo.name)
+            }
+            .bind(to: tableView.rx.items(cellIdentifier: "branchCell", cellType: BranchTableViewCell.self)) {
+                index, branch, cell in
+                cell.branchNameLabel.text = branch.name
+            }.disposed(by: disposeBag)
+    }
 }
